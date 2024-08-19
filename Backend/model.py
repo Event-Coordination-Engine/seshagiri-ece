c8d92d444ee49414cc656600c529fc6fbf085fba
seshagiri <seshagirithananki@gmail.com>
2024-08-19 15:55:17 +0530

    adding new columns and modify

--- Backend/model.py
+++ Backend/model.py
@@ -1,11 +1,14 @@
 from database import Base
 from sqlalchemy import Column,Integer,String
+from datetime import datetime
 
 class User(Base):
     __tablename__="user"
 
     user_id=Column(Integer,primary_key=True)
-    first_name=Column(String,nullable=False)
-    last_name=Column(String,nullable=True)
+    user_name=Column(String,nullable=False)
     email=Column(String,nullable=False)
     password=Column(String,nullable=False)
+    Role=Column(String,nullable=False,default="User")
+    registered_date=Column(datetime, nullable=False,default=datetime.now())
+    
