aca3ce065b46103938f7fa6d1c1feb78d4cd7269
seshagiri <seshagirithananki@gmail.com>
2024-08-20 14:16:39 +0530

    all files proper Execution Test

--- Backend/model.py
+++ Backend/model.py
@@ -1,14 +1,14 @@
 from database import Base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,DateTime
 from datetime import datetime
 
 class User(Base):
     __tablename__="user"
 
-    user_id=Column(Integer,primary_key=True)
-    user_name=Column(String,nullable=False)
-    email=Column(String,nullable=False)
-    password=Column(String,nullable=False)
-    Role=Column(String,nullable=False,default="User")
-    registered_date=Column(datetime, nullable=False,default=datetime.now())
+    user_id = Column(Integer, primary_key = True)
+    user_name = Column(String, nullable = False)
+    email = Column(String, nullable = False)
+    password = Column(String, nullable = False)
+    Role = Column(String, nullable = False, default="User")
+    registered_date = Column(DateTime, nullable=False, default= datetime.now())
     
