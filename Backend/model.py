d78f263674f0089f1bd6c321f273d7dc51c35a72
seshagiri <seshagirithananki@gmail.com>
2024-09-02 22:34:49 +0530

    modify the user column

--- Backend/model.py
+++ Backend/model.py
@@ -9,6 +9,6 @@ class User(Base):
     user_name = Column(String, nullable = False)
     email = Column(String, nullable = False)
     password = Column(String, nullable = False)
-    Role = Column(String, nullable = False, default="User")
+    role = Column(String, nullable = False, default="User")
     registered_date = Column(DateTime, nullable=False, default= datetime.now())
     
